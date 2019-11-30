bc04cfa5f2a4ea688d332d1a60f3df301f4ca3b6
Nitin Jaswal <nathanjaswal@gmail.com>
2019-11-30 13:30:01 -0500

    [Nitin] Landing screen design updated

--- app/src/main/java/com/example/c0773774_f2019_mad3125_midterm/MainActivity.java
+++ app/src/main/java/com/example/c0773774_f2019_mad3125_midterm/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.c0773774_f2019_mad3125_midterm;
 //
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Life Cycle: -
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // call method
+        this.setUpIni();
+
     }
+
+    // Helper: -
+    private void setUpIni() {
+        //
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+    }
+
+    // Action: -
+
+
 }
